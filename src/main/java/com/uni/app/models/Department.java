3a26a6426362e5c4d5acb7485411c1d452340651
BKovalenko <bohdankovalenkowork@gmail.com>
2023-11-06 18:01:07 +0100

    dev: initial hibernate mapping creation

--- src/main/java/com/uni/app/models/Department.java
+++ src/main/java/com/uni/app/models/Department.java
@@ -1,16 +1,28 @@
 package com.uni.app.models;
 
+import jakarta.persistence.*;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 import java.util.List;
 
 @Getter
 @Setter
 @EqualsAndHashCode
+@Entity
+@Table(name = "departments")
 public class Department {
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "departmentSequence")
+  @SequenceGenerator(name = "departmentSequence", sequenceName = "DEPARTMENT_SEQUENCE_PK", initialValue = 1, allocationSize = 20)
+  @Column(name = "id", unique = true, updatable = false, nullable = false)
   private Integer id;
+  @ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.REMOVE}, mappedBy = "departments")
+  @Fetch(FetchMode.SUBSELECT)
   private List<Lecturer> lecturers;
+  @Column(name = "name")
   private String name;
 }
--- src/main/java/com/uni/app/models/Lecturer.java
+++ src/main/java/com/uni/app/models/Lecturer.java
@@ -1,8 +1,11 @@
 package com.uni.app.models;
 
+import jakarta.persistence.*;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 import java.util.List;
 
@@ -10,9 +13,21 @@ import java.util.List;
 @Getter
 @Setter
 @EqualsAndHashCode
+@Entity
+@Table(name = "lecturers")
 public class Lecturer {
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "lecturers_seq")
+  @SequenceGenerator(name = "lecturers_seq", sequenceName = "LECTURERS_SEQUENCE_PK", initialValue = 1, allocationSize = 20)
+  @Column(name = "id", nullable = false, unique = true, updatable = false)
   private Integer id;
+  @ManyToMany(fetch = FetchType.LAZY)
+  @Fetch(FetchMode.SUBSELECT)
+  @JoinTable(name = "departments_lecturers", joinColumns = {@JoinColumn(name = "department_id", nullable = false)}, inverseJoinColumns = {@JoinColumn(name = "lecturer_id", nullable = false)})
   private List<Department> departments;
+  @Enumerated(EnumType.STRING)
+  @Column(name = "degree")
   private Degree degree;
+  @Column(name = "name")
   private String name;
 }
