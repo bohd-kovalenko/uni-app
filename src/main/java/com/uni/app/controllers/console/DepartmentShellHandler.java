06c07b58f6f26fc921bce204bdb1ce42ee5ab802
Bohdan Kovalenko <93287037+bohd-kovalenko@users.noreply.github.com>
2023-11-07 10:16:54 +0100

    Development (#2)
    * dev: fixed path for "search by template" command + small code improvements
    
    * dev: test examples implementation
    
    ---------
    
    Co-authored-by: BKovalenko <bohdankovalenkowork@gmail.com>

--- src/main/java/com/uni/app/controllers/console/DepartmentShellHandler.java
+++ src/main/java/com/uni/app/controllers/console/DepartmentShellHandler.java
@@ -25,9 +25,7 @@ public class DepartmentShellHandler {
     if (!enable) {
       enable = true;
       System.out.println("You successfully enabled shell interaction!");
-    } else {
-      System.out.println("Your shell interaction is already turned in");
-    }
+    } else System.out.println("Your shell interaction is already turned in");
   }
 
   @ShellMethod(key = "Disable S/I", value = "Disable the interaction with an application")
@@ -35,10 +33,8 @@ public class DepartmentShellHandler {
     if (enable) {
       enable = false;
       System.out.println("You successfully disabled shell interaction!");
-    } else {
-      System.out.println("Your shell interaction is already turned off.");
-    }
-  }
+    } else System.out.println("Your shell interaction is already turned off.");
+
 
   @ShellMethod(key = "Show count of employee for department ")
   public void handleCountDepartmentEmployeesCommand(String departmentName) {
@@ -46,10 +42,7 @@ public class DepartmentShellHandler {
       System.out.printf("%d employees found in department %s%n",
           departmentService.findEmployeesCountByDepartmentName(departmentName),
           departmentName);
-    } else {
-      System.out.println(INFORMATION_ABOUT_TURNED_OFF_SHELL);
-    }
-  }
+    } else System.out.println(INFORMATION_ABOUT_TURNED_OFF_SHELL);
 
   @ShellMethod(key = "Show the average salary for the department ")
   public void handleGetAverageDepartmentSalary(String departmentName) {
@@ -57,10 +50,7 @@ public class DepartmentShellHandler {
       System.out.printf("The average salary for the department %s is %f",
           departmentName,
           departmentService.findAverageSalaryByDepartmentName(departmentName));
-    } else {
-      System.out.println(INFORMATION_ABOUT_TURNED_OFF_SHELL);
-    }
-  }
+    } else System.out.println(INFORMATION_ABOUT_TURNED_OFF_SHELL);
 
   @ShellMethod(key = "Who is head of department ")
   public void handleGetDepartmentHead(String departmentName) {
@@ -68,10 +58,7 @@ public class DepartmentShellHandler {
       System.out.printf("The head of department %s is lecturer with the name %s%n",
           departmentName,
           departmentService.findHeadLecturerByDepartmentName(departmentName).getName());
-    } else {
-      System.out.println(INFORMATION_ABOUT_TURNED_OFF_SHELL);
-    }
-  }
+    } else System.out.println(INFORMATION_ABOUT_TURNED_OFF_SHELL);
 
   @ShellMethod(key = "Show statistics of department ")
   public void handleGetDepartmentStat(String departmentName) {
@@ -81,26 +68,19 @@ public class DepartmentShellHandler {
           departmentStat.assistantsCount(),
           departmentStat.associateProfessorsCount(),
           departmentStat.professorsCount());
-    } else {
-      System.out.println(INFORMATION_ABOUT_TURNED_OFF_SHELL);
-    }
+    } else System.out.println(INFORMATION_ABOUT_TURNED_OFF_SHELL);
   }
 
-  @ShellMethod(key = "Global search by")
+  @ShellMethod(key = "Global search by ")
   public void handleGlobalSearchRequest(String template) {
     if (enable) {
       List<Lecturer> lecturers = lecturerService.findNameSurnameCoincidesTemplate(template);
       System.out.printf("Found %d lecturers: \n", lecturers.size());
       lecturers
-          .stream()
           .forEach(lecturer -> System.out.println(lecturer.getName() + " " + lecturer.getSurname()));
       List<Department> departments = departmentService.findDepartmentsNameCoincidesTemplate(template);
       System.out.printf("Found %d departments: \n", departments.size());
       departments
-          .stream()
           .forEach(department -> System.out.println(department.getName()));
-    } else {
-      System.out.println(INFORMATION_ABOUT_TURNED_OFF_SHELL);
-    }
-  }
+    } else System.out.println(INFORMATION_ABOUT_TURNED_OFF_SHELL);
 }
