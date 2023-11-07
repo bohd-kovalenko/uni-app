237970ab8d4325043fec046e22008a23c11c2f29
BKovalenko <bohdankovalenkowork@gmail.com>
2023-11-07 12:11:59 +0100

    dev: minor changes in entities relations loading, added @EntityGraph in repositories

--- src/main/java/com/uni/app/repositories/DepartmentRepository.java
+++ src/main/java/com/uni/app/repositories/DepartmentRepository.java
@@ -1,6 +1,7 @@
 package com.uni.app.repositories;
 
 import com.uni.app.models.Department;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -8,9 +9,11 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface DepartmentRepository extends JpaRepository<Department, Integer> {
+  @EntityGraph(attributePaths = {"lecturers", "headLecturer"})
   @Query("SELECT d FROM Department d WHERE d.name=:name")
   Department findByName(@Param("name") String name);
 
+  @EntityGraph(attributePaths = {"lecturers", "headLecturer"})
   @Query(value = "SELECT d FROM Department d WHERE d.name LIKE %:template%")
   List<Department> findNameCoincides(@Param("template") String template);
 }
--- src/main/java/com/uni/app/repositories/LecturerRepository.java
+++ src/main/java/com/uni/app/repositories/LecturerRepository.java
@@ -1,6 +1,7 @@
 package com.uni.app.repositories;
 
 import com.uni.app.models.Lecturer;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -8,6 +9,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface LecturerRepository extends JpaRepository<Lecturer, Integer> {
+  @EntityGraph(attributePaths = {"departments"})
   @Query(value = "SELECT l FROM Lecturer l WHERE CONCAT(l.name, ' ', l.surname) LIKE %:template%")
   List<Lecturer> findNameSurnameCoincidesTemplate(@Param("template") String template);
 }
